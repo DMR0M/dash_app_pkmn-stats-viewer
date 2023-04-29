24a3044b6005d9247afee4f1a74c1e5a5adb36d7
ROMMEL RUDOLF A. DELA MERCED <rommelrudolfa.delamerced@ROMMELs-MacBook-Air.local>
2023-04-29 15:45:33 +0800

    added histogram and updated search field📊🔎

---
+++ assets/scripts/onEnterSearch.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+// Event listener to pressing ENTER key in input field
+const onEnterSearchPress = (n_clicks, value) => {
+    document.addEventListener(
+        'keydown', () => {
+            if (event.key === 'Enter') {
+                document.getElementById('pkmn-search-btn').click();
+            }
+        });
+    return '';
+};
---
+++ src/components/gen_histogram.py
@@ -0,0 +1,29 @@
+from dash import dcc
+# import dash_bootstrap_components as dbc
+import plotly.express as px
+# ids
+from . import ids
+# data
+from data.data_handler import distributed_data
+
+
+def render(*, pkmn_df=distributed_data()) -> dcc.Graph:
+    hist_chart = px.histogram(
+        pkmn_df, 
+        x='Generation', 
+        title='Number of Pokemon for each Generation',
+        nbins=10,
+        color_discrete_sequence=['indianred'],
+    )
+    hist_chart.update_layout(
+        bargap=0.1,
+        
+        xaxis=dict
+        (
+            tickfont=dict(size=18),
+            
+        )
+    )
+    
+    return dcc.Graph(figure=hist_chart, id=ids.GEN_HIST_GRAPH)
+    
